683be0e790ff516ae9c304fae0ddc691efd582f6
Mateus <mateushbsb@gmail.com>
2021-02-24 10:40:52 -0300

    Surveys module

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,19 +1,18 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { User } from '../models/User';
-
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UsersRepository';
 
 class UserController {
 
-  async create(req: Request, res: Response) {
-    const { name, email } = req.body;
+  async create(request: Request, response: Response) {
+    const { name, email } = request.body;
     
-    const userRepository = getRepository(User);
+    const userRepository = getCustomRepository(UsersRepository);
 
     const findUser = await userRepository.findOne({ email });
 
     if(findUser) {
-      return res.status(400).json({
+      return response.status(400).json({
         error: "User already exists!"
       })
     }
@@ -25,7 +24,7 @@ class UserController {
 
     await userRepository.save(user);
 
-    return res.json(user);
+    return response.json(user);
   }
 }
 
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,11 +1,19 @@
 import { Router } from 'express';
 
 import UserController from '../controllers/UserController';
+import SurveysController from '../controllers/SurveysController';
 
 const router = Router();
 
 const userController = new UserController();
+const surveysController = new SurveysController();
 
+/* Users */
 router.post('/users', userController.create);
+
+/* Surveys */
+
+router.get('/surveys', surveysController.index);
+router.post('/surveys', surveysController.create);
 
 export default router;
