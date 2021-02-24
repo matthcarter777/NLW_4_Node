54d93c8452be394a7708d044046e3ba694f13850
Mateus <mateushbsb@gmail.com>
2021-02-23 21:38:56 -0300

    Models

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
+import 'reflect-metadata';
 import express from 'express';
+
+import router from './routes/routes';
+
+/* Import DB Connect */
+import './database';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({message: 'Ok'});
-});
+app.use(express.json())
+app.use(router);
+
 
 app.listen(3333, () => {
   console.log('API Started on Port 3333!');
