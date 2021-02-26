a417ed5741f2eef78d35736f1817f8b10ace47d4
Mateus <mateushbsb@gmail.com>
2021-02-26 16:19:47 -0300

    Erros

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -1,3 +1,4 @@
+import { AppError } from './../errors/AppError';
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { resolve } from 'path';
@@ -18,17 +19,13 @@ class SendMailController {
     const user = await usersRepository.findOne({email});
 
     if(!user) {
-      return response.status(400).json({
-        error: "User does not exists!",
-      });
+      throw new AppError("User does not exists!");
     }
 
     const survey = await surveysRepository.findOne({id: survey_id});
 
     if(!survey) {
-      return response.status(400).json({
-        error: "Survey does not exist!"
-      });
+      throw new AppError("Survey does not exist!")
     }
 
     const npsPath = resolve(__dirname, '..', 'views', 'emails', 'npsMail.hbs');
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,20 +1,31 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { UsersRepository } from '../repositories/UsersRepository';
+import * as yup from 'yup';
+import { AppError } from './../errors/AppError';
 
 class UserController {
 
   async create(request: Request, response: Response) {
     const { name, email } = request.body;
+
+    const schema = yup.object().shape({
+      name: yup.string().required("Name is required!"),
+      email: yup.string().email("Email").required("Email is required!")
+    });
+
+    try {
+      await schema.validate(request.body, {abortEarly: false})
+    } catch (err) {
+      throw new AppError(err);
+    }
     
     const userRepository = getCustomRepository(UsersRepository);
 
     const findUser = await userRepository.findOne({ email });
 
     if(findUser) {
-      return response.status(400).json({
-        error: "User already exists!"
-      })
+      throw new AppError("User already exists!");
     }
 
     const user = userRepository.create({
